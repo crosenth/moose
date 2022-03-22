45386ee9c95b8afdd2b3c01cc769108f446586f9
Chris Rosenthal <crosenth@uw.edu>
2022-03-22 11:00:56 -0700

    configuring github actions

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setuptools.setup(author='Chris Rosenthal',
                      'Intended Audience :: End Users/Desktop',
                      'License :: OSI Approved :: '
                      'GNU General Public License v3 (GPLv3)',
-                     'Programming Language :: Python :: 3.10',
                      'Programming Language :: Python :: 3.9',
                      'Programming Language :: Python :: 3.8',
                      'Programming Language :: Python :: 3.7',
