80130e8b9e3542cc2d014fbbdea7a09146eb2b0e
Hoogestraat <dhoogest@gattaca.amc.uwmedicine.org>
2023-12-11 22:50:51 -0800

    add conditional to handle scenario where rank was dropped from lineages
    I think that the dropna in line 341 is creating a problem if a dropped
    rank (i.e. species_group) is included ref rank

--- src/classify.py
+++ src/classify.py
@@ -490,15 +490,22 @@ def action(args):
         # Foreach ref rank:
         # - merge with lineages, extract rank_id, rank_name
         for rank in args.include_ref_rank:
-            aligns[rank + '_id'] = aligns.merge(
+            merged = aligns.merge(
                 lineages, left_on='tax_id',
                 right_index=True,
-                how='left')[rank].fillna("")
-            aligns[rank + '_name'] = aligns.merge(
+                how='left')
+            # ensure ref rank wasn't dropped as empty, and fill
+            # with empty strings if it was
+            if rank in merged.columns:
+                aligns[rank + '_id'] = merged[rank]
+                aligns[rank + '_name'] = aligns.merge(
                 lineages,
                 left_on=rank + '_id',
                 right_index=True,
                 how='left')['tax_name_y'].fillna("")
+            else:
+                aligns[rank + '_id'] = ""
+                aligns[rank + '_name'] = ""
 
     # assign seqs that had no results to [no blast_result]
     qseqids = qseqids[~qseqids['qseqid'].isin(aligns['qseqid'])]
